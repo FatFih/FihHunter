f0c2399d72b68ee6b3224b7abc09171ede8edbe3
Czakó Balázs <72661237962@tanulo.bankisuli.hu>
2026-05-26 09:55:12 +0200

    W.I.P Level 1

--- Assets/Scripts/GrapplingAnimation.cs
+++ Assets/Scripts/GrapplingAnimation.cs
@@ -3,13 +3,7 @@ using UnityEngine;
 public class GrapplingAnimation : MonoBehaviour
 {
     public Animator myAnimator;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
